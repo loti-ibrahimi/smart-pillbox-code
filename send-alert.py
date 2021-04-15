a1a9292b0ebc04fdb0a20da5c114ffdb36fd3330
Loti Ibrahimi <loti.ibrahimi@outlook.com>
2021-04-15 15:15:25 +0100

    Added event listener for SMS alerts in the Realtime DB

--- send-alert.py
+++ send-alert.py
@@ -15,61 +15,77 @@ firebase_admin.initialize_app(cred, {
     "databaseURL": "https://thesmartpillbox-ffb73-default-rtdb.firebaseio.com/"
 })
 
+def listener(event):
+    print(event.event_type)  # can be 'put' or 'patch'
+    print(event.path)  # relative to the reference, it seems
+    print(event.data)  # new data at /reference/event.path. None if deleted
+
+
 def main():
     print('')
     print('============================================================')
     print('    Fetching current Alert event data from Realtime DB      ')
     print('============================================================')
     print('')
-    rtdb = db.reference('pillBoxData').child('boxAlerts').get()
-    twilioAlertStatus = rtdb['twilioSMSAlert']
-    if twilioAlertStatus == True:
-        print('* Current Twilio SMS Alert Status:', twilioAlertStatus, '\n (Sending Alert...)')
 
-        # Sample mock data (as if fetched from main schedules DB)
-        pillType = 'PillA'
-        pillQuantity = 1
-        '''
-        <Summary Checklist> 
-        * Check TwilioSMSAlert event in the realtime database
-        * Trigger SMS ALert if TwilioSMSAlert = True.
-        * Set the event to False once Alert is complete.
+    '''
+    <Summary> 
+    * Check TwilioSMSAlert events in the realtime database using event Listener.
+    * Pull down the event data on any new changes made.
+    * Trigger SMS ALert if TwilioSMSAlert = True.
+    * Set the event to False once Alert is complete and SMS is sent.
+    '''
 
-        Cost is 7c per SMS, budget is €14. Roughly 200 SMS.
-        '''
+    # Listen for event changes within pillBoxData > boxAlerts for new event data.
+    db.reference('pillBoxData').child('boxAlerts').listen(listener)
+    while True:
+        time.sleep(0.1)
+        rtdb = db.reference('pillBoxData').child('boxAlerts').get()
+        twilioAlertStatus = rtdb['twilioSMSAlert']
 
-        # Account SID and Token stored in my local env. variable file twilio.env
-        account_sid = os.environ['TWILIO_ACCOUNT_SID']
-        auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        client = Client(account_sid, auth_token)
+        if twilioAlertStatus == True:
+            print('* Current Twilio SMS Alert Status:', twilioAlertStatus, '\n (Sending Alert...)')
 
-        # Text message to be sent.
-        message = client.messages.create(
-            body='Schedule Pill is now Due. \n * Pill Type: '+ pillType +'\n * Quantity: '+ str(pillQuantity) +' \n Please access your Pillbox.',
-            from_='+16124306181',
-            # Stored in my local env. variable file twilio.env
-            to=os.environ['MY_PHONE_NUMBER']
-        )
-        print(message.sid)
+            # # Sample mock data (as if fetched from main schedules DB)
+            # pillType = 'PillA'
+            # pillQuantity = 1
 
-        # Default value for SMS alert event. 
-        twilioAlert = False
-        # Current timestamp.
-        timestamp = datetime.datetime.now().strftime('Time: %H:%M:%S, Date: %d/%m/%Y')
+            # Pull out the Pill Data associated with the recent SMS event from the realtime db.
+            pillDue = rtdb['pillDue']
+            quantityDue = rtdb['quantityDue']
+
+            # Account SID and Token stored in my local env. variable file twilio.env
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            client = Client(account_sid, auth_token)
+
+            # Text message to be sent.
+            message = client.messages.create(
+                body='Schedule Pill is now Due. \n * Pill Type: '+ pillDue +'\n * Quantity: '+ str(quantityDue) +' \n Please access your Pillbox.',
+                from_='+16124306181',
+                # Stored in my local env. variable file twilio.env
+                to=os.environ['MY_PHONE_NUMBER']
+            )
+            print(message.sid)
+
+            # Default value for SMS alert event. 
+            twilioAlert = False
+            # Current timestamp.
+            timestamp = datetime.datetime.now().strftime('Time: %H:%M:%S, Date: %d/%m/%Y')
 
 
-        # Defined data variables to be sent to Firebase.
-        data = {
-            'twilioSMSAlert': twilioAlert,
-            'timestamp': timestamp,
-        }
+            # Defined data variables to be sent to Firebase.
+            data = {
+                'twilioSMSAlert': twilioAlert,
+                'timestamp': timestamp,
+            }
 
-        # Set the Twilio Alert event to False, once the SMS has been sent.
-        print('*! Realtime DB Twilio SMS Alert successfully sent !*')
-        db.reference('/').child('pillBoxData').child('boxAlerts').set(data)
-        # Update the 'history' data under 'boxAccessData', with a history of pushes. 
-        db.reference('/').child('pillBoxData').child('boxAlertMemory').push(data)
-        
+            # Set the Twilio Alert event to False, once the SMS has been sent.
+            print('*! Realtime DB Twilio SMS Alert successfully sent !*')
+            db.reference('/').child('pillBoxData').child('boxAlerts').set(data)
+            # Update the 'history' data under 'boxAccessData', with a history of pushes. 
+            db.reference('/').child('pillBoxData').child('boxAlertMemory').push(data)
+            
     else:
         print('* Current Alert Status:', twilioAlertStatus, '\n (No Alerts triggered)')
     print('')
