d13d549bbcc871be43d3c9dee6c93369fe5243b0
Loti Ibrahimi <loti.ibrahimi@outlook.com>
2021-04-22 12:56:40 +0100

    Data element correction

--- add_update_schedules.py
+++ add_update_schedules.py
@@ -7,6 +7,14 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://thesmartpillbox-ffb73-default-rtdb.firebaseio.com/'
 })
 
+'''
+<Script Overview> 
+* Defining data & creating a document to be added (if document doesn't alrady exist) else update the Firestore DB.
+
+Quick Solution for an ideal CRUD webapp interface - which I didnt get around to.
+
+'''
+
 db = firestore.client()
 
 data = {
@@ -29,11 +37,35 @@ data = {
     {  
         'day': 'Monday',
         'pillCompartment': 4,
-        'pillQunatity': 2,
-        'pillType': 'pill_green',
+        'pillQuantity': 2,
+        'pillType': 'pill_g',
         'schedule_id': 'sched1comp4qt2',
-        'time': '18:30'
-    }
+        'time': '14:30'
+    },
+    {  
+        'day': 'Monday',
+        'pillCompartment': 2,
+        'pillQuantity': 1,
+        'pillType': 'pill_bw',
+        'schedule_id': 'sched2comp2qt1',
+        'time': '14:45'
+    },
+    {  
+        'day': 'Monday',
+        'pillCompartment': 1,
+        'pillQuantity': 2,
+        'pillType': 'pill_w',
+        'schedule_id': 'sched3comp1qt2',
+        'time': '15:30'
+    },
+    {  
+        'day': 'Monday',
+        'pillCompartment': 3,
+        'pillQuantity': 3,
+        'pillType': 'pill_b',
+        'schedule_id': 'sched4comp3qt3',
+        'time': '17:30'
+    },
     ]
 }
 
